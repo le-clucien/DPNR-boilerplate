9315242f28d3b3cc3c8125f5d8ca289112e6474f
Clément Lucien <clement.lucien73@gmail.com>
2022-06-29 13:14:26 +0200

    [bug] Switching to .env part one

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -5,6 +5,7 @@ import { LoggerMiddleware } from './logger.middleware';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
+import { CatsModule } from './cats/cats.module';
 
 @Module( {
 	imports: [
@@ -12,13 +13,14 @@ import { UsersController } from './users/users.controller';
 			type: 'postgres',
 			host: 'pg-server',
 			port: parseInt( process.env.DB_PORT ),
-			username: 'sCtZ2Ufe',
-			password: '8NzXrwuq',
-			database: 'data',
+			username: process.env.PSQL_USER,
+			password: process.env.PSQL_PASS,
+			database: process.env.PSQL_DB_NAME,
 			entities: [ User ],
 			synchronize: true,
 		}),
 		UsersModule,
+		CatsModule,
 	],
 } )
 export class AppModule implements NestModule {
